2416203b384d348d9d4c354527a2386a679cd416
Shyrz <i@shyrz.ch>
2022-05-01 18:49:33 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -16,10 +16,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm791610954-da4cb5acb02c95510031b2d6',
   ],
 
   // 是否显示监测站点的链接
@@ -33,12 +30,12 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '首页',
+      url: 'https://materium.io/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/materium/uptime-status'
     }
   ]
 };
